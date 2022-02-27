216b88dacee90e152bacc0f57f31fcdd20e24270
ubpazhuang <ustczt@mail.ustc.edu.cn>
2022-02-27 11:21:21 +0800

    Feat: add program RGBD8Decode

--- src/App/Programs/RGBD8Decode/main.cpp
+++ src/App/Programs/RGBD8Decode/main.cpp
@@ -7,9 +7,9 @@ using namespace Ubpa;
 using namespace std;
 
 int main(int argc, const char* argv[]) {
-	if (argc != 3) {
+	if (argc != 2) {
 		cerr << "[Error] Command line arguments error." << endl
-			<< "Usage: <input-file-name> <max-value>" << endl;
+			<< "Usage: <input-file-name>" << endl;
 		return -1;
 	}
 	
@@ -17,22 +17,23 @@ int main(int argc, const char* argv[]) {
 
 	Image img(filename);
 
-	float max_value = std::atof(argv[2]);
-
 	const auto w = img.GetWidth();
 	const auto h = img.GetHeight();
 	const auto c = img.GetChannel();
 
 	if (c != 4) {
-		cerr << "[Error] RGBM image must have 4 channels." << endl;
+		cerr << "[Error] RGBD8 image must have 4 channels." << endl;
 		return -1;
 	}
 
 	Image result(w, h, 3);
 
 	for (size_t y = 0; y < h; y++) {
-		for (size_t x = 0; x < w; x++)
-			result.At<rgbf>(x, y) = img.At<rgbaf>(x, y).to_rgb() * max_value;
+		for (size_t x = 0; x < w; x++) {
+			rgbaf src = img.At<rgbaf>(x, y);
+			rgbf decodeColor = src.rgb.to_impl() / ((1.f - (src.a)) * 0.928571403f + 0.071428575f);
+			result.At<rgbf>(x, y) = decodeColor;
+		}
 	}
 	result.Save(filename + ".decode.hdr");
 
